9d9c10b907be11e3783a645f63e8b699f11c7a0b
Giorgi Mosulishvili <121322764+Mosula7@users.noreply.github.com>
2023-11-24 01:03:53 +0400

    minor changes

--- DSHelperFunctions.py
+++ DSHelperFunctions.py
@@ -45,12 +45,13 @@ def classification_predictive_power(y, pred, scoring_func=lambda x: x):
     """
     title_font_size = 14
     table_cmap = sns.cubehelix_palette(start=2, rot=0, dark=0.2, light=1,as_cmap=True)
+    
+    sns.set_style('whitegrid')
+    fig, ax = plt.subplots(2,2, figsize=(18,14))
 
     #PDF
     ks_data = pd.DataFrame({'Target': y, 'prob': pred})
     ks_data['SCORE'] = ks_data['prob'].apply(scoring_func)
-    sns.set_style('whitegrid')
-    fig, ax = plt.subplots(2,2, figsize=(18,14))
     sns.histplot(data=ks_data, x='SCORE', hue='Target',  stat='probability', kde=True, bins=20, common_bins=False, 
                  common_norm=False, palette=['darkorange', 'grey'], ax=ax[0][0], edgecolor='black')
     ax[0][0].set_title('PDF', fontsize=title_font_size)
@@ -76,6 +77,5 @@ def classification_predictive_power(y, pred, scoring_func=lambda x: x):
                  KS - {ks_2samp(ks_data.query('Target == 0')['SCORE'], ks_data.query('Target == 1')['SCORE'])[0]:.4f}\
                  AUC - {roc_auc_score(y, pred):.4f}\
                  Accuracy - {accuracy_score(y, pred>.5):.4f}""", y=.95, fontsize=16)
-    
     plt.show()
 
