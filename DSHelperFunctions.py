d0e7092f7dbb43e7739b7340101e00b9c945481d
Giorgi Mosulishvili <121322764+Mosula7@users.noreply.github.com>
2023-11-24 00:03:36 +0400

    added split function

--- DSHelperFunctions.py
+++ DSHelperFunctions.py
@@ -4,12 +4,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score, accuracy_score, ConfusionMatrixDisplay, classification_report
 from scipy.stats import ks_2samp
 from operator import itemgetter
 
+
+def split_data(df: pd.DataFrame, target: str, test_size: float, val_size: float=None, stratify: bool=None):
+    """
+    returns (X_train, y_train, X_val, y_val, X_test, y_test)
+    """
+    if not val_size:
+        val_size = test_size / (1 - test_size)
+
+    if stratify:
+        stratify_on = df[target]
+    train_val, test = train_test_split(df, test_size=test_size, stratify=stratify_on)
+
+    if stratify:
+        stratify_on = train_val[target]
+    train, val = train_test_split(train_val, test_size=val_size, stratify=stratify_on)
+
+    X_train = train[train.columns.drop(target)]
+    X_val = val[val.columns.drop(target)]
+    X_test = test[test.columns.drop(target)]
+
+    y_train = train[target]
+    y_val = val[target]
+    y_test = test[target]
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
 def classification_predictive_power(y, pred, scoring_func=lambda x: x):
-    
+    """
+    makes 4 plots:
+    first two plots are PDF and CDF of probabilities or scores (if additional scoring/transformation function is provided)
+    the third plot is a confusion matrix and the final plot is a classification report
+    the function also calculates KS statistic, AUC and accuracy
+    """
     title_font_size = 14
     table_cmap = sns.cubehelix_palette(start=2, rot=0, dark=0.2, light=1,as_cmap=True)
 
