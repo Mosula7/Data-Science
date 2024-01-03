3d15fa3f6cb290418674a74f2957bc4670f28a77
Giorgi Mosulishvili <121322764+Mosula7@users.noreply.github.com>
2024-01-04 01:22:44 +0400

    Update DSHelperFunctions.py
    added eda_uni function

--- DSHelperFunctions.py
+++ DSHelperFunctions.py
@@ -10,6 +10,61 @@ from scipy.stats import ks_2samp
 from operator import itemgetter
 
 
+def eda_uni(df, col, dtype='cont'):
+    if dtype == 'cont':
+        sns.set_style('whitegrid')
+
+        mean = df[col].mean()
+        min = df[col].min()
+        max = df[col].max()
+        std = df[col].std()
+        median = df[col].median()
+
+        q1 = df[col].quantile(.25)
+        q3 = df[col].quantile(.75)
+        iqr = q3 - q1
+
+        missing = df[col].isna().sum()
+        outliers = df[(df[col] < q1 - iqr * 1.5) |
+                    (df[col] > q3 + iqr * 1.5)].shape[0]
+
+        n_unique = df[col].nunique()
+        n_bins = 24 if n_unique > 24 else n_unique
+        y_max = np.histogram(df[col].dropna(), bins=n_bins)[0].max()
+
+        fig, ax = plt.subplots(1,2, figsize=(20,7))
+
+        sns.histplot(x=df[col],  bins=n_bins, kde=True, common_bins=False, common_norm=False, color='darkorange', ax=ax[0], edgecolor='black')
+        ax[0].vlines(mean, ymin=0, ymax=y_max, label='mean', color='green', linewidth=3)
+        ax[0].vlines(median, ymin=0, ymax=y_max, label='median', color='red', linewidth=3)
+        ax[0].vlines(q1, ymin=0, ymax=y_max, label='Q1', color='grey', linewidth=3)
+        ax[0].vlines(q3, ymin=0, ymax=y_max, label='Q3', color = 'black', linewidth=3)
+        ax[0].set_title(f'mean = {mean:.2f},   std = {std:.2f},   min = {min:.2f},   max = {max:.2f},   missing = {missing}')
+        ax[0].legend()
+
+        sns.boxplot(data=df[col], ax=ax[1], color='darkorange')
+        ax[1].set_title(f'median = {median:.2f},   Q1 = {q1:.2f},   Q3 = {q3:.2f},   outliers = {outliers}')
+
+        fig.suptitle(col.title(), fontsize=20)
+        plt.show()
+
+
+    if dtype == 'cat':
+        sns.set_style('whitegrid')
+
+        colors = sns.color_palette('pastel')
+        fig, ax = plt.subplots(1, 2, figsize=(20,7))
+        sns.histplot(df[col].fillna('NA'), ax = ax[0], color='orange')
+        ax[0].tick_params(axis='x', labelrotation = 90)
+
+        col_val_count = df[col].value_counts(dropna=False)
+        ax[1].pie(col_val_count, labels=col_val_count.index, colors=colors,  autopct='%1.1f%%')
+        fig.suptitle(col.title(), fontsize=20)
+
+        plt.tight_layout()
+        plt.show()
+
+
 def split_data(df: pd.DataFrame, target: str, test_size: float, val_size: float=None, stratify: bool=None):
     """
     returns (X_train, y_train, X_val, y_val, X_test, y_test)
