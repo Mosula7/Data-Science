df7b121662a9fd2be1a9f9e1f3cbae1a58bab846
Giorgi Mosulishvili <121322764+Mosula7@users.noreply.github.com>
2024-01-04 06:00:56 +0400

    Update DSHelperFunctions.py
    improved split_data added xy_plot

--- DSHelperFunctions.py
+++ DSHelperFunctions.py
@@ -65,20 +65,67 @@ def eda_uni(df, col, dtype='cont'):
         plt.show()
 
 
-def split_data(df: pd.DataFrame, target: str, test_size: float, val_size: float=None, stratify: bool=None):
+def xy_plot(df:pd.DataFrame, *, x:str, y:str, x_type='cont', y_type = 'cat', figsize=(8,7.5)):
+
+  if x_type == 'cont' and y_type == 'cat':
+
+    classes = df[y].unique()
+
+    fig, ax = plt.subplots(2,1, figsize=figsize)
+
+    sns.boxplot(data=df, x=y, y=x, ax=ax[0], palette=sns.color_palette('pastel'))
+
+
+    n_bins = len(np.unique(df[x])) if len(np.unique(df[x])) < 25 else 25
+    sns.histplot(data=df, x=x, hue=y, bins=n_bins, stat="density", edgecolor='black', alpha=.3, ax=ax[1])
+
+    fig.suptitle(x, fontsize=16)
+    plt.tight_layout()
+
+    stats_df = pd.DataFrame(index=classes, columns=['mean', 'std', 'median', 'Q1', 'Q3', 'IQR', 'outliers', 'missing'])
+    sep_y = {class_: df.query(f'{y} == "{class_}"')[x] for class_ in classes}
+
+    for class_, data in sep_y.items():
+      mean = data.mean()
+
+      q1 = data.quantile(.25)
+      q3 = data.quantile(.75)
+      iqr = q3 - q1
+
+      stats_df.loc[class_, 'mean'] = mean
+      stats_df.loc[class_, 'std'] = data.std()
+      stats_df.loc[class_, 'median'] = data.median()
+      stats_df.loc[class_, 'Q1'] = q1
+      stats_df.loc[class_, 'Q3'] = q3
+      stats_df.loc[class_, 'IQR'] = iqr
+      stats_df.loc[class_, 'outliers'] = ((data < q1 - 1.5 * iqr) | (data > q3 + 1.5 * iqr)).sum()
+      stats_df.loc[class_, 'missing'] = data.isna().sum()
+
+    plt.show()
+    display(stats_df)
+    print('-'*105)
+
+  elif x_type == 'cat' and y_type == 'cat':
+    if len(df[col].unique()) > 30:
+      print(f'too many classes in "{x}" try other plots')
+    else:
+
+      fig, ax = plt.subplots(figsize=figsize)
+      sns.countplot(data=df.fillna('NA'), x=x, hue=y, palette=sns.color_palette('pastel'), edgecolor='black', ax=ax)
+      ax.set_title(x, fontsize=16)
+      plt.show()
+
+
+def split_data(df: pd.DataFrame, target: str, test_size: float, 
+               val_size: float=None, random_state:int = 0):
     """
     returns (X_train, y_train, X_val, y_val, X_test, y_test)
     """
     if not val_size:
         val_size = test_size / (1 - test_size)
 
-    if stratify:
-        stratify_on = df[target]
-    train_val, test = train_test_split(df, test_size=test_size, stratify=stratify_on)
-
-    if stratify:
-        stratify_on = train_val[target]
-    train, val = train_test_split(train_val, test_size=val_size, stratify=stratify_on)
+    train_val, test = train_test_split(df, test_size=test_size, stratify=df[target], random_state=random_state)
+    train, val = train_test_split(train_val, test_size=val_size, stratify=train_val[target], random_state=random_state)
 
     X_train = train[train.columns.drop(target)]
     X_val = val[val.columns.drop(target)]
@@ -87,7 +134,7 @@ def split_data(df: pd.DataFrame, target: str, test_size: float, val_size: float=
     y_train = train[target]
     y_val = val[target]
     y_test = test[target]
-
+    
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
